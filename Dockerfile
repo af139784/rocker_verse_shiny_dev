1b0f94ebb1a4d1d9761264c30f553b74a715d605
af139784 <okubo.keisuke@megachips.co.jp>
2021-07-03 22:21:37 +0900

    init

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM af139784/rocker_verse_dev:4.1.0
+
+RUN  /rocker_scripts/install_shiny_server.sh
+
+COPY install_packages.sh  /rocker_scripts/.
+RUN  /rocker_scripts/install_packages.sh
+
---
+++ install_packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# always set this for scripts but don't declare as ENV..
+export DEBIAN_FRONTEND=noninteractive
+
+## Custum install packages
+install2.r  --error -r $CRAN --skipinstalled \
+    shinythemes \
+    shinydashboard \
+    shinyjs \
+    shinyWidgets \
+    shinycssloaders \
+    shinyalert 
+
+# Install from GitHub
+installGithub.r \
+    paulc91/shinyauthr
+
+rm -rf /tmp/downloaded_packages
+rm -rf /var/lib/apt/lists/*
